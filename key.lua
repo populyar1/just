a523321152661267e586c58099313ca5219f8d47
populyar1 <ggg1008ggg108@gmail.com>
2024-11-24 23:53:14 +0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -6,6 +6,7 @@ local me = game:GetService("Players").LocalPlayer
 
 local reallink = false
 local Verified = false
+local format = nil
 
 local loadscript = true
 
@@ -28,20 +29,36 @@ end)
 event:FireServer(true)
 
 local menu = me.PlayerGui:WaitForChild("Scripts")
-local button = menu.Menu.ScrollingFrame.CriminalityGuiV2.GuiV2
+local buttonGuiv2 = menu.Menu.ScrollingFrame.CriminalityGuiV2.GuiV2
+local buttonGuiv2M = menu.Menu.ScrollingFrame.CriminalityGuiV2M.GuiV2M
 
-button.MouseButton1Click:Connect(function()
+buttonGuiv2.MouseButton1Click:Connect(function()
       Verified = true
+      format = "PC"
+end)
+
+buttonGuiv2M.MouseButton1Click:Connect(function()
+      Verified = true
+      format = "Mobile"
 end)
 
 me.OnTeleport:Connect(function()
       if Verified == true then
             if loadscript == true then
                   loadscript = false
-                  queue_on_teleport([[
-                      repeat wait() until game:IsLoaded()
-                      loadstring(game:HttpGet("https://raw.githubusercontent.com/populyar1/just/refs/heads/main/loader.lua"))()
-                  ]])
+                  if format == "PC" then
+                        queue_on_teleport([[
+                            repeat wait() until game:IsLoaded()
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/populyar1/just/refs/heads/main/loader.lua"))()
+                        ]])
+                  elseif format == "Mobile" then
+                        queue_on_teleport([[
+                            repeat wait() until game:IsLoaded()
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/populyar1/just/refs/heads/main/loader2.lua"))()
+                        ]])
+                  else
+                        return nil
+                  end
             end
       end
 end)
